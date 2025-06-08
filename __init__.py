97e462fb18b43c41fa55b731ff901cc5bc4d9cf1
root <seppvr@hotmail.com>
2025-06-08 08:57:20 +0200

    fix setup

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry):
-    await hass.config_entries.async_forward_entry_setups(config_entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, config_entry):
-    return await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    return await hass.config_entries.async_forward_entry_unload(config_entry, ["sensor"])
