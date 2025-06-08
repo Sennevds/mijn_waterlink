ea373781834559a00d86ba22ddc65a78e42f6ca2
root <seppvr@hotmail.com>
2025-06-08 08:43:00 +0200

    async setup

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry):
-    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(config_entry, "sensor")
     return True
 
 async def async_unload_entry(hass, config_entry):
